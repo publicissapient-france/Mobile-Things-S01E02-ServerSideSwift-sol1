01071a3a6314775417a2662f2d3d8f98d153a428
Simone Civetta <viteinfinite@gmail.com>
2017-01-22 00:18:52 +0100

    [dependencies] Add Redbird

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "mts01e02",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
+        .Package(url: "https://github.com/vapor/redis-provider.git", majorVersion: 1, minor: 0)
     ]
 )
